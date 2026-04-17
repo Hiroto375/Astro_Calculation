7f006e6856e84fce1a329a4bd9bc021d86132bcf
Hiroto Hiramatsu <hirotoh0307@docomo.ne.jp>
2026-04-17 18:37:14 +0900

    update

--- N_body.cpp
+++ N_body.cpp
@@ -1,22 +1,15 @@
+// __device__ でGPU上で実行することを命令
+// float3は3次元ベクトルの構造体
+// biは粒子iの位置+質量
+// bjは粒子jの位置+加速度
+// aiは粒子iの現在の加速度
 
-   __device__ float3
-bodyBodyInteraction(float4 bi, float4 bj, float3 ai)
-{
-  float3 r;
-  // r_ij  [3 FLOPS]
-  r.x = bj.x - bi.x;
-  r.y = bj.y - bi.y;
-  r.z = bj.z - bi.z;
-  // distSqr = dot(r_ij, r_ij) + EPS^2  [6 FLOPS]
-   float distSqr = r.x * r.x + r.y * r.y + r.z * r.z + EPS2;
-  // invDistCube =1/distSqr^(3/2)  [4 FLOPS (2 mul, 1 sqrt, 1 inv)]
-   float distSixth = distSqr * distSqr * distSqr;
-  float invDistCube = 1.0f/sqrtf(distSixth);
-  // s = m_j * invDistCube [1 FLOP]
-   float s = bj.w * invDistCube;
-  // a_i =  a_i + s * r_ij [6 FLOPS]
-  ai.x += r.x * s;
-  ai.y += r.y * s;
-  ai.z += r.z * s;
-  return ai;
+__device__ float3 bodyBodyInteraction(float4 bi, float4 bj, float3 ai){
+    // rは粒子iと粒子jの相対位置
+    float3 r;
+
+    r.x = bj.x - bi.x;
+    r.y = bj.y - bi.y;
+    r.z = bj.z - bi.z;
+
 }
